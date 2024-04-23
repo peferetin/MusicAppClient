152881d3cceac6b656b39d0b538c65d69bc5ecbc
peferetin <peferetin@yahoo.com>
2024-04-23 20:52:48 +0200

    added

--- src/MovieRouterApp.jsx
+++ src/MovieRouterApp.jsx
@@ -6,7 +6,8 @@ import NavBar from './components/NavBar.jsx';
 import Loader from './components/Loader.jsx';
 import SearchMovieByName from './components/Search.jsx';
 import AddMovie from './components/AddMovie.jsx';
-import DeleteMovie from './components/DeleteMovie.jsx';
+
+// import DeleteMovie from './components/DeleteMovie.jsx';
 
 const MovieRouterApp = () => {
 
@@ -20,7 +21,8 @@ const MovieRouterApp = () => {
         <Route path="/movies/:_id" element={<Movie />} />
         <Route path="/search" element={<SearchMovieByName />} />
         <Route path="/addMovie" element={<AddMovie />} />
-        <Route path="/deleteMovie" element={<DeleteMovie />} />
+
+        {/* <Route path="/deleteMovie" element={<DeleteMovie />} /> */}
       </Routes>
     </>
   );
